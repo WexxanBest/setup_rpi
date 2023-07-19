6c57c211c44ad59b14101386feb121f72f520132
WexxanBest <kostya.hludeev@gmail.com>
2023-07-19 22:01:20 +0300

    Added test_dlib.py

---
+++ test_dlib.py
@@ -0,0 +1,23 @@
+import cv2
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+
+    for face in faces:
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
